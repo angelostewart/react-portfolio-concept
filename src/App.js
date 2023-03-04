c45a469c5eb1a5a9232de593368123887772398f
Angelo Stewart <sticktology@gmail.com>
2023-03-03 19:35:39 -0500

    completed contact & footer sections

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
         </motion.div>
       </div>
       <LineGradient />
-      {/* <div className="w-5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto md:h-full">
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
@@ -90,7 +90,7 @@ function App() {
           <Contact />
         </motion.div>
       </div>
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
